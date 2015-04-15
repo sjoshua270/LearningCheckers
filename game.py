3a654f7d9f2a9714cc9f2c2e18000777e67754e7
BenderAD <alex.bender@emu.edu>
2015-04-14 22:57:45 -0400

    Copied rungame code to printboard, added global var

--- game.py
+++ game.py
@@ -15,3 +15,13 @@ class Game():
             line += "|"
             print(line)
             print("-------------------------")
+    def print_board(self):
+        global board_values
+        checker_board_dimensions = (8, 8)
+        for x in range(0, checker_board_dimensions[0]):
+            line = ""
+            for y in range(0, checker_board_dimensions[1]):
+                line += "|  "
+            line += "|"
+            print(line)
+            print("-------------------------")
