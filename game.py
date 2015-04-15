bca5b38a85396cf5cd8d4f8b183ec53fa4e9f64f
Joshua Sauder <sjoshua270@gmail.com>
2015-04-14 22:25:24 -0400

    Added basic class

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
 __author__ = 'Joshua'
+
+
+class launch_game():
+    def __init__(self):
+        check_dimens = (8, 8)
+        line = ""
+        for x in range(0, check_dimens[0]):
+            for y in range(0, check_dimens[1]):
+                line += "| "
+            line += "|"
+            print(line)
