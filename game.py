7f42897f49fa11a4efa529ed81eb1e67db22d5b4
BenderAD <alex.bender@emu.edu>
2015-04-15 12:39:48 -0400

    Merge branch 'master' of https://github.com/sjoshua270/LearningCheckers
    Conflicts:
    	game.py

--- game.py
+++ game.py
@@ -19,17 +19,10 @@ class Game():
 
     @staticmethod
     def run_game():
-        global checker_board_dimensions
-        for x in range(0, checker_board_dimensions[0]):
-            line = ""
-            for y in range(0, checker_board_dimensions[1]):
-                line += "|  "
-            line += "|"
-            print(line)
-            print("-------------------------")
+        pass
     def print_board(self):
         global board_values
-        checker_board_dimensions = (8, 8)
+        global checker_board_dimensions
         for x in range(0, checker_board_dimensions[0]):
             line = ""
             for y in range(0, checker_board_dimensions[1]):
