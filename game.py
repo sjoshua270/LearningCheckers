c429bceb1d0e3bf3b5f14aeae43278e76a1c17bf
Joshua Sauder <sjoshua270@gmail.com>
2015-04-14 22:30:20 -0400

    changed a name

--- game.py
+++ game.py
@@ -1,12 +1,12 @@
 __author__ = 'Joshua'
 
 
-class launch_game():
+class LaunchGame():
     def __init__(self):
-        check_dimens = (8, 8)
+        checker_board_dimensions = (8, 8)
         line = ""
-        for x in range(0, check_dimens[0]):
-            for y in range(0, check_dimens[1]):
+        for x in range(0, checker_board_dimensions[0]):
+            for y in range(0, checker_board_dimensions[1]):
                 line += "| "
             line += "|"
             print(line)
