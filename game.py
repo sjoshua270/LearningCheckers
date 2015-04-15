f1e87320316c0c40e643f70d58c582132f09a32b
Joshua Sauder <sjoshua270@gmail.com>
2015-04-14 22:57:14 -0400

    Added board_values

--- game.py
+++ game.py
@@ -2,12 +2,24 @@ __author__ = 'Joshua'
 
 
 class Game():
-
+    checker_board_dimensions = 0
+    board_values = 0
     def __init__(self):
-        pass
-
-    def run_game(self):
+        global checker_board_dimensions
+        global board_values
         checker_board_dimensions = (8, 8)
+        board_values = ((0, 1, 0, 1, 0, 1, 0, 1),
+                        (1, 0, 1, 0, 1, 0, 1, 0),
+                        (0, 1, 0, 1, 0, 1, 0, 1),
+                        (0, 0, 0, 0, 0, 0, 0, 0),
+                        (0, 0, 0, 0, 0, 0, 0, 0),
+                        (2, 0, 2, 0, 2, 0, 2, 0),
+                        (0, 2, 0, 2, 0, 2, 0, 2),
+                        (2, 0, 2, 0, 2, 0, 2, 0))
+
+    @staticmethod
+    def run_game():
+        global checker_board_dimensions
         for x in range(0, checker_board_dimensions[0]):
             line = ""
             for y in range(0, checker_board_dimensions[1]):
